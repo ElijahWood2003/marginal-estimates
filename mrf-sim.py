9bad85f42dc9d67383ab23ef8283141c461286fc
ElijahWood2003 <elijahdwood@gmail.com>
2025-04-06 17:45:11 -0400

    Minor update to mrf-sim

--- mrf-sim.py
+++ mrf-sim.py
@@ -143,7 +143,7 @@ class MarkovRandomField:
                 
         return samples
         
-    # TODO: get 'speed' of function
+    # TODO: track 'speed' of function
     def marginal_probability(self, v: Any, value: Any, num_samples: int = 10000) -> float:
         """
         Estimate marginal probability Pr(X_v = value) using Gibbs sampling.
@@ -250,5 +250,5 @@ for i in range(0, height):
 # Auto propagate the CPTs with random probabilities
 MRF.auto_propagate_cpt()
 
-# Testing marginal probability
+# Testing marginal probability of P(state(0) == 0)
 print(MRF.marginal_probability(0, 0))
