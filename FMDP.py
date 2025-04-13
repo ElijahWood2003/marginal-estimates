c60929f3b11a5b14717befe075a8080917da8833
ElijahWood2003 <elijahdwood@gmail.com>
2025-04-12 21:03:02 -0400

    Updated FMDP

--- FMDP.py
+++ FMDP.py
@@ -30,34 +30,40 @@ class FMDP:
         self._actions = set()         # Actions in FMDP
         self._components = {}         # Map of frozenset{u, v} : [val(u), val(v), direction of token (tuple)]
         self._edges = {}              # Edges dict(action : list of connected actions])
-        self._queue = list()          # Queue of enabled actions
-        self._tokens = {}             # Map of actions -> # of neighbors and # of tokens
+        self._queue = []              # Queue of enabled actions
         self._cpt = {}                # CPT
-        self._values = {}             # Dictionary (action : current value)
+
+        self._values = {}             # Dictionary (action : value)
+        self._tokens = {}             # Dictionary (tuple(u, v) : {0, 1})
+        self._token_count = {}        # Map of actions -> [# of neighbors, # of tokens]  
+
+        # Constants
+        self.LOC_NEIGHBOR_COUNT = 0
+        self.LOC_TOKEN_COUNT = 1
 
     def add_action(self, action: int) -> None:
         """Add an action to the set"""
         self._actions.add(action)
+        self._token_count[action] = [0, 0]
 
     def add_component(self, edge: set, dir: tuple) -> None:
         """Add a component to the map -> set(u, v) : [uval, vval, (u, v)/(v, u)]
         
         Args:
             edge: a set {u, v} representing the edge
-            uval: value of action u
-            vval: value of action v
             dir: direction of token as a tuple (u, v) or (v, u)
         """
-        # As stated above, u < v
+        # As stated in the class notes, for consistency u < v
         u = dir[int(dir[0] > dir[1])]
         v = dir[int(dir[0] < dir[1])]
 
         # Ensure the pointer to the values map is stored, not the value itself
         # This means whenever we update the values within the value map each of the component values will change inherently
-        self._components[edge] = [self._values[u], self._values[v], dir]
+        self._components[frozenset(edge)] = [self._values[u], self._values[v], dir]
 
     def add_edge(self, u: int, v: int) -> None:
-        """Add the connection (u, v) to edges
+        """
+        Add the connection (u, v) to edges
         
         Args:
             u: the input edge
@@ -67,15 +73,64 @@ class FMDP:
             self._edges[u] = []
         
         self._edges[u].append(v)
+
+        # Increment the action's number of neighbors by 1
+        self._token_count[u][self.LOC_NEIGHBOR_COUNT] += 1
     
     def set_value(self, u: int, val: int) -> None:
-        """Set the value of action u to the val
+        """
+        Set the value of action u to the val
         
         Args:
             u: the action we want to set the value of
             val: the value we want to set the action to
         """
         self._values[u] = val
+    
+    def set_actions(self, actions: set) -> None:
+        """Set the actions based on input set"""
+        self._actions = actions
+    
+    def set_edges(self, edges: dict) -> None:
+        """
+        Set the edges based on the input dictionary
+
+        Args:
+            edges: a dictionary of edges assumed to be an adjacency list 
+        """
+        self._edges = edges
+    
+    def set_components(self, tokens: dict) -> None:
+        """
+        Set the components of the FMDP based on 
+        the input tokens
+        
+        Args:
+            tokens: dictionary (tuple(u, v) : {0, 1}) 
+        """
+        for (u, v) in tokens:
+            # Check whether a token exists at this location
+            if(tokens[(u, v)] == 1):
+                self._token_count[v][self.LOC_TOKEN_COUNT] += 1
+
+                # If # of tokens == # of out-neighbors then we add it to the queue of enabled actions
+                if(self._token_count[v][self.LOC_TOKEN_COUNT] == self._token_count[v][self.LOC_NEIGHBOR_COUNT]):
+                    self._queue.append(v)
+
+                # self.add_component({u, v}, (u, v))
+            
+
+    def activate_action(self) -> None:
+        """
+        Activate an action:
+            1. Pop the next action of the queue
+            2. Determine new value of action based on CPT
+            3. Set _token_count[action][token_count] = 0
+            4. For each out-neighbor, increase their # of tokens by 1 and check for new enabled action 
+        """
+        action = self._queue.pop(0)
+    
+
 
 
 # TODO: joint distribution estimates by fixing strategy of FMDP and sampling sufficiently long paths
